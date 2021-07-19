fe81d3456fe01d00ef7fb559cb7cc2e4d2e7d6c7
rlucila438@gmail.com <rlucila438@gmail.com>
2021-07-19 02:00:13 -0300

    se agrega servidor web

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require ('express');
+const app = express ();
+const path =  require ('path');
+const port = 3030;
+
+app.listen (port, ()=> console.log(`Servidor corrienddo en http://localhost:${port}`));
+app.use (express.static('public'));
+
+app.get ('/', (req, res)=>res.send('Bienvenidos a mi sitio web'));
+app.get ('/home', (req, res)=> res.sendFile(path.join(__dirname, 'views', 'index.html')));
+app.get ('/babbage', (req, res)=> res.sendFile(path.join(__dirname, 'views', 'babbage.html')));
+app.get ('/berners-lee', (req, res)=> res.sendFile(path.join(__dirname, 'views', 'berners-lee.html')));
+app.get ('/clarke', (req, res)=> res.sendFile(path.join(__dirname, 'views', 'clarke.html')));
+app.get ('/hamilton', (req, res)=> res.sendFile(path.join(__dirname, 'views', 'hamilton.html')));
+app.get ('/hopper', (req, res)=> res.sendFile(path.join(__dirname, 'views', 'hopper.html')));
+app.get ('/lovelace', (req, res)=> res.sendFile(path.join(__dirname, 'views', 'lovelace.html')));
+app.get ('/turing', (req, res)=> res.sendFile(path.join(__dirname, 'views', 'turing.html')));
